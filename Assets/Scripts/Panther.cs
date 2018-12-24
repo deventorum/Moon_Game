bffea6afde1751113c9a281d05cf642285807ea9
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-12-24 14:54:56 -0500

    Yiles

--- Assets/Scripts/Panther.cs
+++ Assets/Scripts/Panther.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Panther : Humanoid
+public class Panther : MonoBehaviour
 {
   // Start is called before the first frame update
   void Start()
@@ -11,8 +11,13 @@ public class Panther : Humanoid
   }
 
   // Update is called once per frame
-  void Update()
+  void FixedUpdate()
   {
+    float h = Input.GetAxisRaw("Horizontal");
+    float v = Input.GetAxisRaw("Vertical");
 
+    Vector3 tempVect = new Vector3(h, 0, v);
+    tempVect = tempVect.normalized * speed * Time.deltaTime;
+    rb.MovePosition(transform.position + tempVect);
   }
 }
