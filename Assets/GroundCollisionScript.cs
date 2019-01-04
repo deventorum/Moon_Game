0a7bc00aa4d9d46a798e997a53f12342012a1647
deventorum <deventorum@gmail.com>
2019-01-04 14:58:37 -0500

    Spacemen is being fixed

---
+++ Assets/GroundCollisionScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCollisionScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    public Collider[] groundCol;
+
+    private void Awake()
+    {
+        // add isTrigger
+        var boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = true;
+    }
+    void Start()
+    {
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Space_Daddy.cs
+++ Assets/Scripts/Space_Daddy.cs
@@ -1,8 +1,10 @@
+using System;
 using UnityEngine;
 using System.Collections;
 
 public class Space_Daddy : MonoBehaviour {
     private Animator anim;
+    private Animation animate;
     private Rigidbody rigidBody;
     private CharacterController controller;
     [SerializeField] float jumpForce = 20f;
@@ -12,33 +14,120 @@ public class Space_Daddy : MonoBehaviour {
     private Vector3 moveDirection = Vector3.zero;
     public float gravity = 20.0f;
     public Vector3 jump;
+    bool groundedState;
+    public Collider[] colliderBoxes;
 
     void Start () {
         controller = GetComponent <CharacterController>();
         anim = gameObject.GetComponentInChildren<Animator>();
+        animate = gameObject.GetComponentInChildren<Animation>();
+        groundedState = CheckGroundCollision(colliderBoxes[0]);
     }
 
     void Update (){
-        if (Input.GetKey ("w"))
+
+        if (Input.GetKey ("w") || Input.GetKey("s"))
         {
-            anim.SetInteger ("AnimationPar", 1);
+
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("Run"))
+            {
+                // do something
+                anim.SetInteger("AnimationPar", 0);
+            }
+            else
+            {
+                anim.SetInteger("AnimationPar", 1);
+            }
         }
         else
         {
             anim.SetInteger("AnimationPar", 0);
         }
-        if(controller.isGrounded)
+        if(groundedState)
         {
             moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
             Jumping();
         }
-        Flip();
+            Flip();
 
         float turn = Input.GetAxis("Horizontal");
             transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
             controller.Move(moveDirection * Time.deltaTime);
             moveDirection.y -= gravity * Time.deltaTime;
     
+    }
+    void FixedUpdate()
+    {
+        SetGroundedState();
+    }
+
+    private void SetGroundedState()
+    {
+        groundedState = CheckGroundCollision(colliderBoxes[0]);
+        //CheckGroundCollision(colliderBoxes[0]);
+        CheckAttackCollision(colliderBoxes[1]);
+        Debug.Log(groundedState);
+    }
+
+
+    private bool CheckGroundCollision(Collider col)
+    {
+        Collider[] cols = Physics.OverlapBox(col.transform.position, col.transform.localScale / 2, Quaternion.identity, LayerMask.GetMask("Terrain", "Base"));
+        {
+            foreach (Collider c in cols)
+            {
+                //groundedState |= c is UnityEngine.MeshCollider;
+                Debug.Log(c.name + gameObject);
+                //groundedState = true;
+
+            }
+
+            if (cols.Length > 0)
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+
+
+    private void CheckAttackCollision(Collider col)
+    {
+
+    }
+
+    //void OnTriggerEnter(Collider col)
+    //{
+    //    Debug.Log("Hit");
+    //    if (col is BoxCollider && col.name != "Stylized Astronaut")
+    //    {
+    //        if (col.gameObject.tag == "GroundCollider")
+    //        {
+    //            Debug.Log(col.gameObject.name + " colliding with " + col.name + " when rigidbody is " + col.attachedRigidbody);
+    //        }
+    //        if (col.gameObject.tag == "HurtBox")
+    //        {
+    //            Debug.Log(col.gameObject.name + " colliding with " + col.name + " when rigidbody is " + col.attachedRigidbody);
+    //        }
+    //    }
+    //    if (col.name != "Stylized Astronaut")
+    //    {
+    //        Debug.Log("Object " + name + tag + gameObject.gameObject +" has hit object "+ col.name);
+    //    }
+    //    if (null != col.gameObject.GetComponent<Sword>())
+    //    {
+    //        //do sword stuff
+    //    }
+    //}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        //Check that it is being run in Play Mode, so it doesn't try to draw this in Editor mode
+            //Draw a cube where the OverlapBox is (positioned where your GameObject is as well as a size)
+            Gizmos.DrawWireCube(transform.position, transform.localScale / 2);
     }
 
     private void Jumping()
@@ -51,7 +140,7 @@ public class Space_Daddy : MonoBehaviour {
     }
     private void Flip()
     {
-        if (Input.GetKeyDown("f") && !controller.isGrounded)
+        if (Input.GetKeyDown("f") && !groundedState)
         {
             anim.Play("Flip");
         }
