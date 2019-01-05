19e8f3d4f9dba8b79b0cca37ee3bd2add3f97870
deventorum <deventorum@gmail.com>
2019-01-04 20:20:05 -0500

    Animation improved, added control in midair

--- Assets/Scripts/Space_Daddy.cs
+++ Assets/Scripts/Space_Daddy.cs
@@ -4,51 +4,99 @@ using System.Collections;
 
 public class Space_Daddy : MonoBehaviour {
     private Animator anim;
-    private Animation animate;
     private Rigidbody rigidBody;
     private CharacterController controller;
     [SerializeField] float jumpForce = 20f;
 
+    private Vector3 jumpVelocity = Vector3.zero;
+    private Vector3 sideManeuring = Vector3.zero;
+
     public float speed = 600.0f;
     public float turnSpeed = 400.0f;
-    private Vector3 moveDirection = Vector3.zero;
+    public float sidewayControl = 20.0f;
+
+    //private Vector3 moveDirection = Vector3.zero;
+
+
     public float gravity = 20.0f;
     public Vector3 jump;
     private bool groundedState;
     public Collider[] colliderBoxes;
     private static readonly int AnimationPar = Animator.StringToHash("AnimationPar");
+    private 
 
-    private void Start () {
+    void Start () {
         controller = GetComponent <CharacterController>();
         anim = gameObject.GetComponentInChildren<Animator>();
-        animate = gameObject.GetComponentInChildren<Animation>();
         groundedState = CheckGroundCollision(colliderBoxes[0]);
     }
 
     private void Update (){
 
+
+
         if (Input.GetKey ("w") || Input.GetKey("s"))
         {
-            anim.SetInteger(AnimationPar, anim.GetCurrentAnimatorStateInfo(0).IsName("Run") ? 0 : 1);
+            anim.SetInteger(AnimationPar, 1);
         }
         else
         {
             anim.SetInteger(AnimationPar, 0);
         }
-        if(groundedState)
+
+        
+        Vector3 moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+        moveDirection = transform.TransformDirection(moveDirection);
+
+        if (groundedState)
         {
-            moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
-            Jumping();
+            moveDirection *= speed;
+            if (Input.GetKeyDown("space"))
+            {
+                jumpVelocity = moveDirection;
+                jumpVelocity.y = jumpForce;
+                anim.Play("Jump_start");
+            }
+            else
+            {
+                //sideManeuring = moveDirection;
+                //sideManeuring.x = sidewayControl;
+                jumpVelocity = Vector3.zero;
+
+            }
         }
-            Flip();
-        if (!groundedState)
+        else
         {
-            anim.Play("Jump_loop");
+            if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Flip"))
+            {
+                anim.Play("Jump_loop");
+            }
+            moveDirection *= speed / 2;
+            jumpVelocity.y -= gravity * Time.deltaTime;
         }
+        Flip();
         var turn = Input.GetAxis("Horizontal");
-            transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
-            controller.Move(moveDirection * Time.deltaTime);
-            moveDirection.y -= gravity * Time.deltaTime;
+        transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+        controller.Move((moveDirection + jumpVelocity) * Time.deltaTime);
+
+        //if (groundedState)
+        //{
+
+        //    moveDirection = transform.forward * Input.GetAxis("Vertical") * speed;
+        //    Jumping();
+        //}
+        //    Flip();
+        //if (!groundedState)
+        //{
+        //    if (!anim.GetCurrentAnimatorStateInfo(0).IsName("Flip"))
+        //    {
+        //        anim.Play("Jump_loop");
+        //    }
+        //}
+        //var turn = Input.GetAxis("Horizontal");
+            //transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+            //controller.Move(moveDirection * Time.deltaTime);
+            //moveDirection.y -= gravity * Time.deltaTime;
     
     }
     private void FixedUpdate()
@@ -70,13 +118,11 @@ public class Space_Daddy : MonoBehaviour {
     {
         var transform1 = col.transform;
         Collider[] cols = Physics.OverlapBox(transform1.position, transform1.localScale / 2, Quaternion.identity, LayerMask.GetMask("Terrain", "Base"));
+        foreach (Collider c in cols)
         {
-            foreach (Collider c in cols)
-            {
-                Debug.Log(c.name + gameObject);
-            }
-
+            Debug.Log(c.name + gameObject);
         }
+
         if (cols.Length > 0 && !groundedState)
         {
             anim.Play("Jump_end");
@@ -97,12 +143,12 @@ public class Space_Daddy : MonoBehaviour {
         Gizmos.DrawWireCube(transform1.position, transform1.localScale / 2);
     }
 
-    private void Jumping()
-    {
-        if (!Input.GetKeyDown("space")) return;
-        anim.Play("Jump_start");
-        moveDirection.y += jumpForce;
-    }
+    //private void Jumping()
+    //{
+    //    if (!Input.GetKeyDown("space")) return;
+    //    anim.Play("Jump_start");
+    //    moveDirection.y += jumpForce;
+    //}
     private void Flip()
     {
         if (Input.GetKeyDown("f") && !groundedState)
