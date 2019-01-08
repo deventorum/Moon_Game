b023e3131ca937c659e6ba31443708d3a601502e
nohkes <amandeepsekhon404@gmail.com>
2019-01-08 12:42:50 -0500

    first push and added level

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -40,4 +40,8 @@ public class GameController : MonoBehaviour
     {
         SceneManager.LoadScene(gameOverScene);
     }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
 }
